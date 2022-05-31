1e3b82f7de4d0fb22b735b647f4391df74d893b9
Alexander Bolotskov <mamoruuu@proton.me>
2022-05-31 22:43:20 +0400

    chore(root): 🧰 git-cz config

---
+++ changelog.config.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const commitTypes = [
+	{
+		description: 'Fresh new feature',
+		emoji: '🚀',
+		value: 'feat'
+	},
+	{
+		value: 'improve',
+		emoji: '👷',
+		description: 'Code that improves something'
+	},
+	{
+		description: 'Bug fix',
+		emoji: '🐛',
+		value: 'fix'
+	},
+	{
+		description: 'Code change that neither fixes a bug or adds a feature',
+		emoji: '💡',
+		value: 'refactor'
+	},
+	{
+		description: 'CI/CD related changes',
+		emoji: '👷',
+		value: 'ci'
+	},
+	{
+		description: 'Build process or auxiliary tool changes',
+		emoji: '🧰',
+		value: 'chore'
+	},
+	{
+		description: 'Markup, white-space, formatting, missing semi-colons...',
+		emoji: '🧁',
+		value: 'style'
+	},
+]
+
+const types = {}
+
+for (const type of commitTypes) {
+	types[type.value] = type
+}
+
+module.exports = {
+	disableEmoji: false,
+	list: commitTypes.map(type => type.value),
+	maxMessageLength: 64,
+	minMessageLength: 3,
+	questions: [
+		'type',
+		'scope',
+		'subject',
+		'body',
+	],
+	scopes: ['root', 'front', 'api'],
+	types
+}
