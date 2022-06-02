5d9df04fda83d776f67bfb6a2d5307e6e74d5d5b
Alexander Bolotskov <mamoruuu@proton.me>
2022-06-02 20:07:04 +0400

    refactor(api): 💡 file structure

---
+++ apps/api/src/const.ts
@@ -0,0 +1,2 @@
+export const defaultPort = 3500
+export const port = process.env.PORT ? +process.env.PORT : defaultPort
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,24 +1,25 @@
 import 'reflect-metadata'
-import * as express from 'express'
+
 import { database } from './database'
+import { port } from './const'
+import { app } from './app'
+import { endpoints } from './endpoints'
 
-const port = 3500
-const app = express()
-
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
 	res.send('Hello world!')
 })
+
+app.use('/v1', endpoints)
 
 database
 	.initialize()
 	.then(() => {
 		console.log('MongoDB connection has been established')
+		return app.listen(port, () => {
+			console.log(`API listening on port ${port}`)
+		})
 	})
 	.catch(err => {
 		console.error('An error occured during connecting to MongoDB', err)
 		process.exit(1)
 	})
-
-app.listen(port, () => {
-	console.log(`API listening on port ${port}`)
-})
