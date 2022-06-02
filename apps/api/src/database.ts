fdb30bcdd7f1df08cb258868719974f02d2db24e
Alexander Bolotskov <mamoruuu@proton.me>
2022-06-02 19:58:30 +0400

    feat(api): 🚀 mongodb connection

---
+++ apps/api/src/database.ts
@@ -0,0 +1,14 @@
+import { DataSource } from 'typeorm'
+
+export const database = new DataSource({
+	type: 'mongodb',
+	// TODO: user definable parameters
+	host: 'localhost',
+	username: 'root',
+	password: 'example',
+	database: 'boards',
+	synchronize: true,
+	logging: true,
+	appname: 'boards/api',
+	authSource: 'admin',
+})
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,6 @@
-import * as express from "express";
+import 'reflect-metadata'
+import * as express from 'express'
+import { database } from './database'
 
 const port = 3500
 const app = express()
@@ -6,6 +8,16 @@ const app = express()
 app.get('/', (req, res) => {
 	res.send('Hello world!')
 })
+
+database
+	.initialize()
+	.then(() => {
+		console.log('MongoDB connection has been established')
+	})
+	.catch(err => {
+		console.error('An error occured during connecting to MongoDB', err)
+		process.exit(1)
+	})
 
 app.listen(port, () => {
 	console.log(`API listening on port ${port}`)
