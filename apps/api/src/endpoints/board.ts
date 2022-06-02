2c3f20f1b2690216b453b8175f2b7c4b66cce7b4
Alexander Bolotskov <mamoruuu@proton.me>
2022-06-02 21:56:56 +0400

    feat(api): 🚀 post endpoint for boards creation

--- apps/api/src/endpoints/board.ts
+++ apps/api/src/endpoints/board.ts
@@ -4,10 +4,22 @@ import { Board, IBoard, IListDTO } from '../model'
 
 export const board = express()
 
-board.get('/', async (_, res) => {
-	const boards = await database.getRepository(Board).find()
-	res.json({
-		list: boards,
-		size: boards.length,
-	} as IListDTO<IBoard>)
-})
+board
+	.route('/')
+	.get(async (_, res) => {
+		const boards = await database.getRepository(Board).find()
+		return res.status(200).json({
+			list: boards,
+			size: boards.length,
+		} as IListDTO<IBoard>)
+	})
+	.post(async (req, res) => {
+		try {
+			const board = await database.getRepository(Board).create(req.body)
+			const results = await database.getRepository(Board).save(board)
+			res.status(201).json(results)
+		} catch (e) {
+			console.error('error', e)
+			res.status(500).json(e)
+		}
+	})
