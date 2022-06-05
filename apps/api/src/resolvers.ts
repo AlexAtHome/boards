fb45852374dc89145c1a63473d22d4678668de9c
Alexander Bolotskov <mamoruuu@proton.me>
2022-06-05 20:11:09 +0400

    feat(api): 🚀 adds the mutation for creating new boards

--- apps/api/src/resolvers.ts
+++ apps/api/src/resolvers.ts
@@ -1,7 +1,11 @@
+import { CreateBoardParams } from '@boards/types'
 import { BoardService } from './services/boards.service'
 
 export const resolvers = {
 	Query: {
 		boards: () => BoardService.getList(),
+	},
+	Mutation: {
+		createBoard: (_: unknown, params: CreateBoardParams) => BoardService.create(params),
 	},
 }
--- apps/api/src/type-defs.ts
+++ apps/api/src/type-defs.ts
@@ -2,18 +2,22 @@ import { gql } from 'apollo-server'
 
 export const typeDefs = gql`
 	type Board {
-		id: String
+		id: ID
 		name: String
 		createdAt: String
 		updatedAt: String
 	}
 
 	type Query {
-		boards: BoardListResponse
+		boards: BoardListResponse!
+	}
+
+	type Mutation {
+		createBoard(name: String): Board
 	}
 
 	type BoardListResponse {
-		list: [Board]
+		list: [Board!]!
 		size: Int
 	}
 `
