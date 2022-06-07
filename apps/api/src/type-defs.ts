03c679dcfc995fff68fe16efbeb0f9e263ba8c27
Alexander Bolotskov <mamoruuu@proton.me>
2022-06-07 23:52:29 +0400

    feat(api): 🚀 adds type definitions for Tasks in GraphQL schema

--- apps/api/src/type-defs.ts
+++ apps/api/src/type-defs.ts
@@ -2,24 +2,58 @@ import { gql } from 'apollo-server'
 
 export const typeDefs = gql`
 	type Board {
-		uuid: ID
+		uuid: ID!
 		name: String
 		createdAt: String
 		updatedAt: String
+		tasks: TaskListResponse
 	}
 
 	type Query {
 		boards: BoardListResponse!
 		board(uuid: ID): Board
+
+		task(uuid: ID): Task
+		tasks(boardUuid: ID): TaskListResponse!
 	}
 
 	type Mutation {
 		createBoard(name: String): Board!
 		editBoard(uuid: ID, name: String): Board!
+
+		createTask(input: CreateTaskInput): Task!
+		editTask(input: EditTaskInput): Task!
+		deleteTask(input: DeleteTaskInput): Task!
 	}
 
 	type BoardListResponse {
 		list: [Board!]!
 		size: Int
+	}
+
+	type TaskListResponse {
+		list: [Task!]
+		size: Int
+	}
+
+	type Task {
+		uuid: ID!
+		name: String
+		description: String
+	}
+
+	input CreateTaskInput {
+		name: String!
+		description: String
+	}
+
+	input EditTaskInput {
+		name: String
+		description: String
+	}
+
+	input DeleteTaskInput {
+		name: String
+		description: String
 	}
 `
