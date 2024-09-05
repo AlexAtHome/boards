4eaa752ab4aaea4768962d1586accd91c0bb7ef3
Alex Bolotskov <mamoruuu@proton.me>
2024-09-05 22:42:44 +0500

    feat: adds a max width to the task list

--- src/pages/task.page.jsx
+++ src/pages/task.page.jsx
@@ -1,7 +1,7 @@
 import CreateTaskForm from '../components/create-task-form'
 import TaskList from '../components/task-list'
 
-export const TaskPage = () => <>
+export const TaskPage = () => <div className="flex flex-col gap-3 w-full max-w-5xl mx-auto">
 	<CreateTaskForm />
 	<TaskList />
-</>
+</div>
