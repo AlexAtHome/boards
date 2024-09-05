8f54aa9f4e99b809aa11016a486b7e6338491b32
Alex Bolotskov <mamoruuu@proton.me>
2024-09-05 22:45:42 +0500

    fix: task appearance in light colorscheme

--- src/components/task-list.jsx
+++ src/components/task-list.jsx
@@ -30,7 +30,7 @@ const TaskList = () => {
 /** @param {TaskProps} task */
 const TaskItem = (task) => {
 	const id = `task-${task.id}`
-	return <li key={task.id} aria-labelledby={id} className="bg-slate-700 rounded-md inline-flex justify-between items-center">
+	return <li key={task.id} aria-labelledby={id} className="bg-slate-300 dark:bg-slate-700 rounded-md inline-flex justify-between items-center">
 		<span id={id} className={clsx("py-2 px-4", { "line-through opacity-50": task.isDone })}>{task.title}</span>
 		{!task.isDone && <button type="button" title="Mark as done" onClick={() => task.onMarkAsDone(task)} className="p-2 inline-flex justify-center items-center focus:outline outline-2 outline-green-600 dark:outline-green-300 outline-offset-2 rounded-md disabled:cursor-not-allowed disabled:opacity-50">
 			<CheckCircleIcon role="presentation" className="w-6" />
