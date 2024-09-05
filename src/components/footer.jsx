bc366300754ac0e139e34dbf4c27a05e94582a05
Alex Bolotskov <mamoruuu@proton.me>
2024-09-05 22:45:20 +0500

    feat: privacy policy page link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,10 @@
-const Footer = () => <footer className="h-12 mt-auto p-3 flex gap-2 items-center bg-slate-50 dark:bg-slate-800 sticky bottom-0">
+import { Link } from "react-router-dom"
+
+const Footer = () => <footer className="h-12 mt-auto p-3 flex gap-2 items-center justify-between bg-slate-50 dark:bg-slate-800 sticky bottom-0">
 	<span>{new Date().getFullYear()} &copy; <a href="https://alexalex.dev" target='_blank'>Alex Bolotskov</a></span>
+	<span>
+		<Link to="/privacy-policy" className="text-blue-400 underline underline-offset-1 hover:text-blue-300">Privacy Policy</Link>
+	</span>
 </footer>
 
 export default Footer
