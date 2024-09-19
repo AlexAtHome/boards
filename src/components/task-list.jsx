0791422a553414ebe6b930ac567e0bc59b4a24b2
Alex Bolotskov <mamoruuu@proton.me>
2024-09-19 22:54:42 +0500

    feat: shows task descriptions and metadata on the list

--- src/components/task-list.jsx
+++ src/components/task-list.jsx
@@ -2,6 +2,7 @@ import { clsx } from 'clsx';
 import PropTypes from 'prop-types'
 import { useAppSelector } from "../hooks"
 import { CheckCircleIcon } from '@heroicons/react/24/solid'
+import { formatDistance, format } from 'date-fns'
 import { useAppDispatch } from '../hooks'
 import { selectAllTasks, selectTasksStatus, selectTasksError, markTaskAsDone } from '../features/task-list';
 
@@ -29,8 +30,25 @@ const TaskList = () => {
 /** @param {TaskProps} task */
 const TaskItem = (task) => {
 	const id = `task-${task.id}`
-	return <li key={task.id} aria-labelledby={id} className="bg-slate-300 dark:bg-slate-700 rounded-md inline-flex justify-between items-center">
-		<span id={id} className={clsx("py-2 px-4", { "line-through opacity-50": task.isDone })}>{task.title}</span>
+	return <li key={task.id} aria-labelledby={id} className="bg-slate-300 dark:bg-slate-700 rounded-md inline-flex justify-between items-start">
+		<details className="inline-flex grow">
+			<summary className="marker:text-transparent py-2 cursor-default"><span id={id} className={clsx({ "line-through opacity-50": task.isDone })}>{task.title}</span></summary>
+			<div className="px-4 pb-4">
+				{task.description && <div className="text-sm">{
+					task.description.split('\n').map((line, i) => <p key={i}>{line}</p>)
+				}</div>}
+				<dl className="flex flex-col gap-1 text-xs mt-4">
+					<div className="inline-flex gap-1">
+						<dt>Created at</dt>
+						<dd>{format(task.createdAt, 'PPP')}</dd>
+					</div>
+					{task.createdAt !== task.modifiedAt && <div className="inline-flex gap-1">
+						<dt>Modified at</dt>
+						<dd>{task.modifiedAt}</dd>
+					</div>}
+				</dl>
+			</div>
+		</details>
 		{!task.isDone && <button type="button" title="Mark as done" onClick={() => task.onMarkAsDone(task)} className="p-2 inline-flex justify-center items-center focus:outline outline-2 outline-green-600 dark:outline-green-300 outline-offset-2 rounded-md disabled:cursor-not-allowed disabled:opacity-50">
 			<CheckCircleIcon role="presentation" className="w-6" />
 		</button>}
